34a31c87f87177da0f6943ed166e304fb19662f5
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-07-10 10:31:29 +0100

    JOIN

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -237,6 +237,79 @@ SELECT continent, COUNT(name) FROM world WHERE population/1000000>10 GROUP BY co
 -- Counting big continents 8. List the continents that have a total population of at least 100 million.
 SELECT continent FROM world GROUP BY continent HAVING SUM(population/1000000)>100
 
+-- **********************************************************************************************************************************
+-- ************************************************************* JOIN ***************************************************************
+-- **********************************************************************************************************************************
+
+-- 1. Modify it to show the matchid and player name for all goals scored by Germany
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER'
+  
+-- 2.Show id, stadium, team1, team2 for just game 1012
+SELECT id,stadium,team1,team2
+  FROM game WHERE id = 1012
+
+-- 3. Modify it to show the player, teamid, stadium and mdate for every German goal.
+SELECT goal.player, goal.teamid, game.stadium, game.mdate 
+  FROM game JOIN goal ON (goal.matchid=game.id) INNER JOIN eteam ON (eteam.id=goal.teamid) WHERE eteam.teamname = 'Germany'
+
+-- 4. Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT game.team1, game.team2, goal.player
+  FROM game JOIN goal ON (goal.matchid=game.id) WHERE goal.player LIKE 'Mario%'
+
+-- 5. Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT goal.player, goal.teamid, eteam.coach, goal.gtime
+  FROM goal JOIN eteam ON eteam.id = goal.teamid
+ WHERE gtime<=10
+
+-- 6. List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+SELECT game.mdate, eteam.teamname
+  FROM game JOIN eteam ON game.team1=eteam.id
+ WHERE eteam.coach= 'Fernando Santos'
+
+-- 7. List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+SELECT goal.player
+  FROM goal JOIN game ON game.id=goal.matchid
+ WHERE game.stadium= 'National Stadium, Warsaw'
+
+-- 8. More difficult questions Instead show the name of all players who scored a goal against Germany.
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id
+  WHERE (team1='GER' OR team2 = 'GER') AND goal.teamid != 'GER'
+
+-- 9. Show teamname and the total number of goals scored.
+SELECT teamname, COUNT(goal.matchid)
+  FROM eteam JOIN goal ON eteam.id=goal.teamid
+ GROUP BY teamname
+
+-- 10. Show the stadium and the number of goals scored in each stadium.
+ SELECT game.stadium, COUNT(goal.matchid)
+  FROM game JOIN goal ON game.id=goal.matchid
+ GROUP BY stadium
+
+-- 11. For every match involving 'POL', show the matchid, date and the number of goals scored.
+SELECT matchid,MAX(game.mdate) AS mdate, COUNT(teamid)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY goal.matchid
+
+-- 12. For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+SELECT matchid, MAX(mdate), COUNT(teamid)
+  FROM game JOIN goal ON matchid = id 
+ WHERE teamid = 'GER' GROUP BY goal.matchid
+
+-- 13. List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
+SELECT mdate,
+  team1, SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+  team2, SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+  FROM game LEFT JOIN goal ON matchid = id GROUP BY mdate,matchid,team1,team2
+
+-- **********************************************************************************************************************************
+-- ************************************************************* More JOIN **********************************************************
+-- **********************************************************************************************************************************
+
+
+
+
 
 
 
